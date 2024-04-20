c4dc3a69f0005074e2a8a5b6b62c89cde7697f26
sskycn <sskycn@gmail.com>
2024-04-20 20:52:15 +0800

    v0.0.5

--- validator.go
+++ validator.go
@@ -15,8 +15,52 @@ var (
 	regexDomain   = regexp.MustCompile(strDomain)
 
 	regexEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
-	regexPhone = regexp.MustCompile(`^[1-9][0-9]{10}$`)
+	regexPhone = regexp.MustCompile(`^[1-9][0-9]{10,11}$`)
 )
+
+// Int validate name
+func Int(name string, val int) error {
+
+	if val == 0 {
+		return fmt.Errorf("%s invalid", name)
+	}
+
+	return nil
+}
+
+// Uint64 validate name
+func Uint64(name string, val uint64) error {
+
+	if val == 0 {
+		return fmt.Errorf("%s invalid", name)
+	}
+
+	return nil
+}
+
+// String validate name
+func String(name string, val string) error {
+
+	if val == "" {
+		return fmt.Errorf("%s required", name)
+	}
+
+	return nil
+}
+
+// RString validate name
+func RString(name string, val *string) error {
+
+	if val == nil {
+		return fmt.Errorf("%s invalid", name)
+	}
+
+	if *val == "" {
+		return fmt.Errorf("%s required", name)
+	}
+
+	return nil
+}
 
 // ValidateName validate name
 func ValidateName(name ...string) error {
