688ceb3f97b10e5d5a73dca25dff1fa5f1c79a54
sskycn <sskycn@gmail.com>
2024-04-21 09:49:10 +0800

    clean

--- error.go
+++ error.go
@@ -3,6 +3,14 @@ package utils
 import "errors"
 
 var (
+	// ErrEmailOrPasswordInvalid email or password invalid
+	ErrEmailOrPasswordInvalid = errors.New("email or password invalid")
+	// ErrEmailNotVerified email not verified
+	ErrEmailNotVerified = errors.New("email not verified")
+	// ErrPhoneOrPasswordInvalid phone or password invalid
+	ErrPhoneOrPasswordInvalid = errors.New("phone or password invalid")
+	// ErrPhoneNotVerified phone not verified
+	ErrPhoneNotVerified = errors.New("phone not verified")
 	// ErrDecodingPemBlock decoding PEM block error
 	ErrDecodingPemBlock = errors.New("decoding PEM block error")
 	// ErrConvertingEcPublicKey converting EC public key error
