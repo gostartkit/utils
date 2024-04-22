cb1950c6edf4a69fc5a5d17f0b9ee721b129646c
sskycn <sskycn@gmail.com>
2024-04-22 09:35:08 +0800

    clean

--- error.go
+++ error.go
@@ -7,10 +7,14 @@ var (
 	ErrEmailOrPasswordInvalid = errors.New("email or password invalid")
 	// ErrEmailNotVerified email not verified
 	ErrEmailNotVerified = errors.New("email not verified")
+	// ErrEmailAddressIsAlreadyInUse eemail address is already in use
+	ErrEmailAddressIsAlreadyInUse = errors.New("email address is already in use")
 	// ErrPhoneOrPasswordInvalid phone or password invalid
 	ErrPhoneOrPasswordInvalid = errors.New("phone or password invalid")
 	// ErrPhoneNotVerified phone not verified
 	ErrPhoneNotVerified = errors.New("phone not verified")
+	// ErrPhoneNumberIsAlreadyInUse phone number is already in use
+	ErrPhoneNumberIsAlreadyInUse = errors.New("phone number is already in use")
 	// ErrDecodingPemBlock decoding PEM block error
 	ErrDecodingPemBlock = errors.New("decoding PEM block error")
 	// ErrConvertingEcPublicKey converting EC public key error
