bac60318badefa7372ac42c5196ddb6a5b213c71
sskycn <sskycn@gmail.com>
2024-07-08 17:12:36 +0800

    clean

--- browser.go
+++ browser.go
@@ -7,20 +7,17 @@ import (
 
 // OpenBrowser attempts to open the specified URL in the default browser of the user.
 func OpenBrowser(url string) error {
-	var cmd string
-	var args []string
+
+	var cmd *exec.Cmd
 
 	switch runtime.GOOS {
 	case "darwin":
-		cmd = "open"
-		args = []string{url}
+		cmd = exec.Command("open", url)
 	case "windows":
-		cmd = "rundll32"
-		args = []string{"url.dll,FileProtocolHandler", url}
+		cmd = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
 	default: // Unix-like systems: "linux", "freebsd", "openbsd", "netbsd"
-		cmd = "xdg-open"
-		args = []string{url}
+		cmd = exec.Command("xdg-open", url)
 	}
 
-	return exec.Command(cmd, args...).Run()
+	return cmd.Run()
 }
