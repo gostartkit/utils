302cdaed0f2748475896b73baa5e96a671705076
afxcn <afxcn@gzitech.com>
2025-06-17 11:21:17 +0800

    make uri more safe

--- browser.go
+++ browser.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"os/exec"
 	"runtime"
 	"strings"
@@ -8,6 +9,10 @@ import (
 
 // OpenBrowser attempts to open the specified URL in the default browser of the user.
 func OpenBrowser(uri string) error {
+
+	if !isValidURI(uri) {
+		return fmt.Errorf("invalid URI: %s", uri)
+	}
 
 	switch runtime.GOOS {
 	case "darwin":
@@ -17,7 +22,7 @@ func OpenBrowser(uri string) error {
 	default: // Unix-like systems: "linux", "freebsd", "openbsd", "netbsd"
 		if err := exec.Command("xdg-open", uri).Run(); err != nil {
 			// wsl
-			uri = strings.Replace(uri, "&", "^&", -1)
+			uri = escapeForCmd(uri)
 			if err2 := exec.Command("cmd.exe", "/C", "start", "chrome", uri).Run(); err2 != nil {
 				if err3 := exec.Command("cmd.exe", "/C", "start", uri).Run(); err3 != nil {
 					return err
@@ -26,4 +31,23 @@ func OpenBrowser(uri string) error {
 		}
 		return nil
 	}
+}
+
+func escapeForCmd(uri string) string {
+	// Escape CMD special characters: &, |, >, <
+	replacements := map[string]string{
+		"&": "^&",
+		"|": "^|",
+		">": "^>",
+		"<": "^<",
+	}
+	result := uri
+	for old, new := range replacements {
+		result = strings.ReplaceAll(result, old, new)
+	}
+	return result
+}
+
+func isValidURI(uri string) bool {
+	return strings.HasPrefix(uri, "http://") || strings.HasPrefix(uri, "https://")
 }
